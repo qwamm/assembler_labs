e3baacf36823534066cdc0b6806066afd8013323
afonin.is <anonimmail123@bk.ru>
2024-02-29 23:30:14 +0300

    overflow processing implemented

--- lab1/lab1.s
+++ lab1/lab1.s
@@ -5,7 +5,7 @@ section .data
 	b_len: equ $-b
 	c: dd 1
 	c_len: equ $-c
-	d: dw 1
+	d: dw 0
 	d_len: equ $-d
 	e: dd 2
 	e_len: equ $-e
@@ -20,27 +20,35 @@ _start:
 	movzx ebx, word [d]
 	mov ecx, dword[e]
 	mul ebx
+	jc error
 	mul ecx
+	jc error
 	mov dword[res], eax
 	mov eax, dword [a]
 	movzx ebx, word [b]
 	mov ecx, dword[c]
 	mul ebx
+	jc error
 	mul ecx
+	jc error
 	mov ebx, dword[res]
 	sub eax,ebx
+	jc error
 	mov dword[res], eax
 	mov eax, dword [a]
 	movzx ebx, word [b]
+	test ebx, ebx
 	jz error
 	div ebx
 	mov dword[e], eax
 	mov eax, dword [c]
 	movzx ebx, word [d]
+	test ebx, ebx
 	jz error
 	div ebx
 	mov ebx, dword[e]
 	add eax, ebx
+	jc error
 	mov ebx, eax
 	mov eax, dword[res]
 	div ebx

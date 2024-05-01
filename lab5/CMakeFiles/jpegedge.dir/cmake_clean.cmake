f6dc08b1f9f7c19ac6ef84bbde8f1ce2bb0d09c5
afonin.is <anonimmail123@bk.ru>
2024-05-01 21:32:50 +0300

    jpeg processing

---
+++ lab5/CMakeFiles/jpegedge.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/jpegedge.dir/jpegedge.c.o"
+  "CMakeFiles/jpegedge.dir/jpegedge.c.o.d"
+  "jpegedge"
+  "jpegedge.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/jpegedge.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
